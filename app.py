89ae3c985961348c32d545ecfd90033763b15aee
Felipe Delmondes <luis.delmondes@outlook.com>
2022-06-19 12:29:19 -0300

    Criação de tela de login com PySimpleGUI

---
+++ app.py
@@ -0,0 +1,28 @@
+import PySimpleGUI as sg
+
+
+layout = [
+    [sg.Text('Usuario')],
+    [sg.Input(key='usuario')],
+    [sg.Text('Senha')],
+    [sg.Input(key='senha')],
+    [sg.Button('login'),sg.Button('fechar')],
+    [sg.Text('',key='mensagem')],
+]
+
+
+janela = sg.Window('login',layout=layout)
+
+while True:
+    event, values = janela.read()
+    if event == sg.WIN_CLOSED or event == 'fechar':
+        break
+    elif event == 'login':
+        senha_correta = 'fe270495'
+        usuario_correto = 'ludelmondes'
+        usuario = values['usuario']
+        senha = values['senha']
+        if senha == senha_correta and usuario == usuario_correto:
+            janela['mensagem'].update('Login realizado com sucesso')
+        else:
+            janela['mensagem'].update('Usuario ou senha invalidos')
